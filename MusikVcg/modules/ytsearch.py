b43b44c96ce3738f30b599aa1f23cf8a6d09a9ac
Allstartsoflegend <89901205+Allstartsoflegend@users.noreply.github.com>
2021-09-04 18:29:49 +0700

    Update ytsearch.py

--- MusikVcg/modules/ytsearch.py
+++ MusikVcg/modules/ytsearch.py
@@ -36,10 +36,10 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 async def ytsearch(_, message: Message):
     try:
         if len(message.command) < 2:
-            await message.reply_text("/search ketikan judul yg kalian cari")
+            await message.reply_text("/search Ketikan judul yg kalian cari")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("Searching....")
+        m = await message.reply_text("Mencari lagu....")
         results = YoutubeSearch(query, max_results=4).to_dict()
         i = 0
         text = ""
