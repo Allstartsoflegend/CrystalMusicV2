a7dec5670dd0f198d9aea1e3ddf765c1abc5fbb3
Allstartsoflegend <89901205+Allstartsoflegend@users.noreply.github.com>
2021-09-04 20:00:07 +0700

    Update song.py

--- MusikVcg/modules/song.py
+++ MusikVcg/modules/song.py
@@ -35,8 +35,8 @@ from pyrogram.types import Message
 from youtube_search import YoutubeSearch
 from youtubesearchpython import SearchVideos
 
-from MusikVcg.config import DURATION_LIMIT
-from MusikVcg.modules.play import arq
+from CrystalMusic.config import DURATION_LIMIT
+from CrystalMusic.modules.play import arq
 
 
 @Client.on_message(filters.command("song") & ~filters.channel)
